9a4a268b37b5b9f62567113e162ed087ef329d04
Son Nguyen Hoang <sonnguyen9800@gmail.com>
2025-09-28 12:55:00 +0700

    + improve

--- main.go
+++ main.go
@@ -19,6 +19,8 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 
+	"github.com/gin-contrib/cors"
+
 	"errors"
 )
 
@@ -90,6 +92,17 @@ func main() {
 	tagHandler := tag.NewHandler(client, config)
 	// Initialize Gin router
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins: []string{
+			"http://localhost:8090", // flutter run web default
+			"https://mydomain.com",  // production - must configurable by docker compose
+		},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	// Public routes
 	r.GET("/api/ping", func(c *gin.Context) {
